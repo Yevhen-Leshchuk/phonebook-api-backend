19840e14d106dd40de4d94b66e59b8955129ffb7
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2023-08-11 22:04:51 +0300

    Add:Contacts Schema, update contacts.js.

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,16 +1,15 @@
 /* eslint-disable array-callback-return */
 // const fs = require('fs').promises;
-const ObjectId = require('mongodb').ObjectId;
+const { Contact } = require('../src/db/postModel');
 
 const getContacts = async (req, res) => {
-  const contacts = await req.db.Contacts.find({}).toArray();
+  const contacts = await Contact.find({});
   res.json({ contacts });
 };
 
 const getContactById = async (req, res) => {
   const { id } = req.params;
-  const contacts = await req.db.Contacts.findOne({ _id: new ObjectId(id) });
-
+  const contacts = await Contact.findById(id);
   if (!contacts) {
     res.status(404).json({ message: 'Not found' });
   }
@@ -22,30 +21,25 @@ const addContact = async (req, res) => {
     res.status(400).json({ message: 'Missing required field' });
   }
   const { name, email, phone, favorite } = req.body;
-
-  await req.db.Contacts.insertOne({ name, email, phone, favorite });
-
+  const contact = new Contact({ name, email, phone, favorite });
+  await contact.save();
   res.status(201).json({ message: 'Success' });
 };
 
 const updateContact = async (req, res) => {
   const { name, email, phone, favorite } = req.body;
   const { id } = req.params;
-
   if (name && email && phone && favorite) {
-    await req.db.Contacts.updateOne(
-      { _id: new ObjectId(id) },
-      { $set: { name, email, phone, favorite } }
-    );
+    await Contact.findByIdAndUpdate(id, {
+      $set: { name, email, phone, favorite },
+    });
   }
-
   res.status(200).json({ message: 'Success' });
 };
 
 const removeContact = async (req, res) => {
   const { id } = req.params;
-  await req.db.Contacts.deleteOne({ _id: new ObjectId(id) });
-
+  await Contact.findByIdAndRemove(id);
   res.status(200).json({ message: 'Contact deleted' });
 };
 
---
+++ src/db/postModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const contactSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Set name for contact'],
+    unique: true,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contact = mongoose.model('Contacts', contactSchema);
+
+module.exports = {
+  Contact,
+};
