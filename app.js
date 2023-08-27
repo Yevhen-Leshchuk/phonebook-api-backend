8c655b5729cafd8c90ed7d811b9e7ffe83aa00e9
Yevhen-Leshchuk <evg.leschuk@gmail.com>
2023-08-27 23:22:51 +0300

    Add:authRouter.js.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const { contactsRouter } = require('./src/routers/contactsRouter');
+const { authRouter } = require('./src/routers/authRouter');
 const { errorHandler } = require('./src/helpers/apiHelpers');
 
 const app = express();
@@ -14,6 +15,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 app.use('/api/contacts', contactsRouter);
+app.use('/api/auth', authRouter);
 
 app.use((req, res) => {
   res.status(404).json({ message: 'Not found' });
---
+++ src/routers/authRouter.js
@@ -0,0 +1,17 @@
+/* eslint-disable object-curly-spacing */
+const express = require('express');
+const router = new express.Router();
+
+const { asyncWrapper } = require('../helpers/apiHelpers');
+
+const {
+  signupController,
+  loginController,
+} = require('../controllers/authController');
+
+router.post('/signup', asyncWrapper(signupController));
+router.post('/login', asyncWrapper(loginController));
+
+module.exports = {
+  authRouter: router,
+};
